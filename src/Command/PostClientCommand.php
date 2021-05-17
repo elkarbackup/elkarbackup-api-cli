8601d1634e95597c9bd8828a1f3e66b021d7b604
uanabitarte <uanabitarte@localhost>
2021-05-17 12:53:08 +0200

    Add options to createClient command

--- src/Command/PostClientCommand.php
+++ src/Command/PostClientCommand.php
@@ -5,33 +5,67 @@ use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\HttpClient\HttpClient;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputOption;
 
 class PostClientCommand extends Command
 {
+
     protected function configure()
     {
-        $this
-        ->setName('PostClient')
-        ->setDescription('Create client')
-        ;
+        $this->setName('client:create')
+            ->setDescription('Create client')
+            ->addOption('name', null, InputOption::VALUE_REQUIRED, "Client's name")
+            ->addOption('description', null, InputOption::VALUE_OPTIONAL)
+            ->addOption('isActive', null, InputOption::VALUE_OPTIONAL, "Client is active", true)
+            ->addOption('maxParallelJobs', null, InputOption::VALUE_OPTIONAL, '', 1)
+            ->addOption('owner', null, InputOption::VALUE_REQUIRED, "Client's owner")
+            ->addOption('postScripts', null, InputOption::VALUE_OPTIONAL | InputOption::VALUE_IS_ARRAY, '', [])
+            ->addOption('preScripts', null, InputOption::VALUE_OPTIONAL | InputOption::VALUE_IS_ARRAY, '', [])
+            ->addOption('quota', null, InputOption::VALUE_OPTIONAL, '', - 1)
+            ->addOption('rsyncLongArgs', null, InputOption::VALUE_OPTIONAL)
+            ->addOption('rsyncShortArgs', null, InputOption::VALUE_OPTIONAL)
+            ->addOption('sshArgs', null, InputOption::VALUE_OPTIONAL)
+            ->addOption('url', null, InputOption::VALUE_OPTIONAL)
+            ->addArgument('outputFile', InputArgument::OPTIONAL);
     }
-    
+
     protected function execute(InputInterface $input, OutputInterface $output)
     {
         $httpClient = HttpClient::create();
+        $json = [
+            'description' => $input->getOption('description'),
+            'isActive' => $input->getOption('isActive'),
+            'maxParallelJobs' => $input->getOption('maxParallelJobs'),
+            'name' => $input->getOption('name'),
+            'owner' => (int)$input->getOption('owner'),
+            'postScripts' => $input->getOption('postScripts'),
+            'preScripts' => $input->getOption('preScripts'),
+            'quota' => $input->getOption('quota'),
+            'rsyncLongArgs' => $input->getOption('rsyncLongArgs'),
+            'rsyncShortArgs' => $input->getOption('rsyncShortArgs'),
+            'sshArgs' => $input->getOption('sshArgs'),
+            'url' => $input->getOption('url')
+        ];
         $response = $httpClient->request('POST', 'http://127.0.0.1/api/clients', [
-            'auth_basic' => ['root', 'root'],
-            'json' => [
-                'description' => 'description',
-                'isActive' => true,
-                'maxParallelJobs' => 1,
-                'name' => 'clientCLI',
-                'owner' => 1,
-                'quota' => -1,
-                'url' => 'root@172.17.0.1'
-            ]
+            'auth_basic' => [
+                'root',
+                'root'
+            ],
+            'json' => $json
         ]);
-        $output->writeln($response->getContent());
+        if (201 == $response->getStatusCode()) {
+            $output->writeln("Client created successfully");
+        } else {
+            $output->writeln($response->getInfo());
+        }
+        $outputFilename = $input->getArgument('outputFile');
+        if ($outputFilename) {
+            $file = fopen($outputFilename, 'w');
+            fwrite($file, $response->getContent());
+        } else {
+            $output->writeln($response->getContent());
+        }
     }
 }
 
