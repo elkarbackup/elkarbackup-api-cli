1c33cf270bdded4ac279f17ba727f614dee4ca9f
uanabitarte <uanabitarte@localhost>
2021-05-17 12:52:34 +0200

    Add output file to GetClients

--- src/Command/GetClientsCommand.php
+++ src/Command/GetClientsCommand.php
@@ -5,14 +5,16 @@ use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\HttpClient\HttpClient;
+use Symfony\Component\Console\Input\InputArgument;
 
 class GetClientsCommand extends Command
 {
     protected function configure()
     {
         $this
-        ->setName('GetClients')
+        ->setName('client:list')
         ->setDescription('Gets client list')
+        ->addArgument('file', InputArgument::OPTIONAL, "Output file for the clients' list")
         ;
     }
     
@@ -20,7 +22,15 @@ class GetClientsCommand extends Command
     {
         $httpClient = HttpClient::create();
         $response = $httpClient->request('GET', 'http://127.0.0.1/api/clients', ['auth_basic' => ['root', 'root'],]);
-        $output->writeln($response->getContent());
+        $output->writeln("Get clients");
+        $filename = $input->getArgument('file');
+        if ($filename) {
+            $file = fopen($filename, 'w');
+            fwrite($file, $response->getContent());
+            fclose($file);
+        } else {
+            $output->writeln($response->getContent());
+        }
     }
 }
 
