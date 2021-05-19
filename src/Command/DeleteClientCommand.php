84a02aa77494b7cd9de6a36d5cb823c078b8cf39
uanabitarte <u.anabitarte@gmail.com>
2021-05-19 16:17:21 +0200

    DeleteClientCommand extends BaseCommand and check if id is int

--- src/Command/DeleteClientCommand.php
+++ src/Command/DeleteClientCommand.php
@@ -1,13 +1,12 @@
 <?php
 namespace App\Command;
 
-use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\HttpClient\HttpClient;
 use Symfony\Component\Console\Input\InputArgument;
 
-class DeleteClientCommand extends Command
+class DeleteClientCommand extends BaseCommand
 {
     protected function configure()
     {
@@ -19,14 +18,14 @@ class DeleteClientCommand extends Command
         ->addArgument('id', InputArgument::REQUIRED, "Client's id")
         ->addArgument('url', InputArgument::OPTIONAL, "Url of the api", "http://127.0.0.1");
     }
-    
+
     protected function execute(InputInterface $input, OutputInterface $output)
     {
         $httpClient = HttpClient::create();
         $url = $input->getArgument('url');
         $username = $input->getArgument('username');
         $password = $input->getArgument('password');
-        $id = $input->getArgument('id');
+        $id = $this->parseInt($input->getArgument('id'));
         $output->writeln("Delete client ".$id);
         $response = $httpClient->request('DELETE', $url.'/api/clients/'.$id, ['auth_basic' => [$username, $password],]);
         if (204 == $response->getStatusCode()){
